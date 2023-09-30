3b2ebfcc45ac46a1c8401e45fe4b8673d9288fbe
KZ211 <alexis.jares12@gmail.com>
2023-09-30 20:42:10 -0300

    Agregue la carpeta completa del juego descomprimida del gfar

---
+++ combate-criaturas/Pikachu2.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pikachu2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pikachu2 extends Criatura{
+     public Pikachu2(String nombre, boolean imagenEspejada) {
+        super(nombre, 21, new String[] { "Placaje", "- Vacío -", "- Vacío -", "- Vacío -" }, imagenEspejada,
+                new String[] { "Causa un daño moderado a un enemigo", "-", "-", "-" });
+    }
+    
+    
+    public Pikachu2(String nombre) {
+        this(nombre, false);
+    }
+
+    public void atacar2(Criatura otro) {
+        atacar1(otro);
+    }
+
+    public boolean puedeRealizarAtaque2En(Criatura otro) {
+        return false;
+    }
+
+    public void atacar3(Criatura otro) {
+        atacar1(otro);
+    }
+
+    public boolean puedeRealizarAtaque3En(Criatura otro) {
+        return false;
+    }
+
+    public void atacar4(Criatura otro) {
+        atacar1(otro);
+    }
+
+    public boolean puedeRealizarAtaque4En(Criatura otro) {
+        return false;
+    }
+}
